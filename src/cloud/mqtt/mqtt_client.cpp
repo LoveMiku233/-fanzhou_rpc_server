7db6481d7d548f0d3bbde03bbbe06f223e26d762
严凯 泛舟 <yan@fanzhou.cloud>
2026-01-13 10:45:10 +0800

    add cloud_1

--- src/cloud/mqtt/mqtt_client.cpp
+++ src/cloud/mqtt/mqtt_client.cpp
@@ -1,8 +1,79 @@
 #include "mqtt_client.h"
 #include <QtMqtt/qmqttclient.h>
 
-//mqtt_client::mqtt_client()
-//{
-//    auto m_client = new QMqttClient(this);
+namespace fanzhou {
+namespace cloud {
 
-//}
+MqttClient::MqttClient(QObject *parent)
+    : QObject(parent),
+      m_client(new QMqttClient(this))
+{
+    connect(m_client, &QMqttClient::connected,
+            this, &MqttClient::onConnected);
+
+    connect(m_client, &QMqttClient::disconnected,
+            this, &MqttClient::onDisconnected);
+
+    connect(m_client, &QMqttClient::messageReceived,
+            this, &MqttClient::onMessageReceived);
+}
+
+
+void MqttClient::setBroker(const QString &host, quint16 port)
+{
+    m_client->setHostname(host);
+    m_client->setPort(port);
+}
+
+void MqttClient::connectToBroker()
+{
+    qDebug() << "MQTT connecting to"
+             << m_client->hostname()
+             << m_client->port();
+    m_client->connectToHost();
+}
+
+void MqttClient::disconnectFromBroker()
+{
+    m_client->disconnectFromHost();
+}
+
+void MqttClient::publish(const QString &topic,
+                         const QByteArray &payload,
+                         int qos)
+{
+    if (m_client->state() == QMqttClient::Connected) {
+        m_client->publish(topic, payload, qos);
+    }
+}
+
+void MqttClient::subscribe(const QString &topic, int qos)
+{
+    if (m_client->state() == QMqttClient::Connected) {
+        m_client->subscribe(topic, qos);
+    }
+}
+
+void MqttClient::onConnected()
+{
+    qDebug() << "MQTT connected";
+}
+
+void MqttClient::onDisconnected()
+{
+    qDebug() << "MQTT disconnected";
+}
+
+void MqttClient::onMessageReceived(const QByteArray &message,
+                                   const QMqttTopicName &topic)
+{
+    qDebug() << "MQTT message:"
+             << topic.name()
+             << message;
+}
+
+
+}
+}
+
+
--- src/cloud/mqtt/mqtt_client.h
+++ src/cloud/mqtt/mqtt_client.h
@@ -2,10 +2,35 @@
 #define MQTT_CLIENT_H
 
 #include <QObject>
+#include <QtMqtt/qmqttclient.h>
 
 namespace fanzhou {
 namespace cloud{
-//
+
+class MqttClient : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit MqttClient(QObject *parent = nullptr);
+
+    // setBroker
+    void setBroker(const QString &host, quint16 port = 1883);
+    void connectToBroker();
+    void disconnectFromBroker();
+
+    void publish(const QString &topic, const QByteArray &payload, int qos = 0);
+    void subscribe(const QString &topic, int qos = 0);
+
+private slots:
+    void onConnected();
+    void onDisconnected();
+    void onMessageReceived(const QByteArray &message,
+                           const QMqttTopicName &topic);
+private:
+    QMqttClient *m_client;
+};
+
 
 }
 }
